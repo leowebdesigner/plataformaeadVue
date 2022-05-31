fa354ab30d9e082808419e0bb0fab2ec64e2ae82
leowebdesigner <contato@leowebdesigner.com.br>
2022-05-31 00:51:31 -0300

    Finish Modules and Lessons

---
+++ src/views/modules/components/Player.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="training">
+        <div class="card bg-laravel">
+            <span class="icon">
+                <img src="images/icons/laravel.svg" alt="">
+            </span>
+            <span class="title">Aula 01 - Leonardo</span>
+            <!--<a href="trainings.html" class="btn laravel">
+                <i class="fas fa-chevron-left"></i>
+                Voltar
+            </a>-->
+            <router-link :to="{ name: 'campus.home' }" :class="['btn', 'laravel', 'fas', 'fa-chevron-left']">
+                Voltar
+            </router-link>
+        </div>
+
+        <iframe width="100%" height="auto" src="https://www.youtube.com/embed/tC7_0X6oS-Q" title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Player"
+}
+</script>

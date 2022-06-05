24d46f7c232f1a5d0cff24519a430e4aa7883d63
leowebdesigner <contato@leowebdesigner.com.br>
2022-06-05 00:10:17 -0300

    Toggle Menu Click

--- src/views/modules/components/Modules.vue
+++ src/views/modules/components/Modules.vue
@@ -5,13 +5,20 @@
                 <span class="text">Modulos</span>
                 <span class="icon far fa-stream"></span>
             </div>
-            <div class="modules" v-for="module in modules" :key="module.id">
+            <div 
+            v-for="module in modules" 
+            :key="module.id"
+            @click.prevent ="toogleModule(module.id)"
+            :class="[
+                'modules',
+                module.id == showModule ? 'active' : ''
+            ]">
                 <div class="name">
                     <span class="text">{{module.name}}</span>
                     <span class="icon fas fa-sort-down"></span>
                 </div>
-                <ul class="classes" v-for="lesson in module.lessons" :key="lesson.id">
-                    <li>
+                <ul class="classes" v-show="module.id == showModule">
+                    <li v-for="lesson in module.lessons" :key="lesson.id">
                         <span v-if="lesson.views.length > 0" class="check active fas fa-check"></span>
                         <span class="nameLesson">{{lesson.name}}</span>
                     </li>
@@ -23,19 +30,28 @@
 
 <script>
 import { useStore } from 'vuex'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
 export default {
     name: 'Modules',
     setup(){
       const store = useStore()
 
+      const showModule = ref('0')
+
       //const course = computed(() => store.state.courses.courseSelected)
        const modules = computed(() => store.state.courses.courseSelected.modules)
+
+       const toogleModule = (moduleId) => {
+           showModule.value = moduleId
+
+       }
 
       return{ 
           //course,
           modules,
+          showModule,
+          toogleModule,
       }
     },
     
